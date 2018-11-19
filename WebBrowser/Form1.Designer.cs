9f53dbb75a62c9b8f1b6a802f5b84ad06e63a5de
Altughan Ozengi <31768522+altugozengi@users.noreply.github.com>
2018-11-19 17:42:09 +0100

    files uploaded

---
+++ WebBrowser/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿using System;
+
+namespace WebBrowser
+{
+    partial class Browser_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Browser_Form));
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.ProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.NavStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.goForward = new System.Windows.Forms.Button();
+            this.goBck = new System.Windows.Forms.Button();
+            this.Refresher = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(12, 67);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScriptErrorsSuppressed = true;
+            this.webBrowser.Size = new System.Drawing.Size(987, 439);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.webBrowser_ProgressChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1011, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem1});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // optionsToolStripMenuItem1
+            // 
+            this.optionsToolStripMenuItem1.Name = "optionsToolStripMenuItem1";
+            this.optionsToolStripMenuItem1.Size = new System.Drawing.Size(116, 22);
+            this.optionsToolStripMenuItem1.Text = "Options";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ProgressBar,
+            this.NavStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 521);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1011, 26);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(200, 20);
+            // 
+            // NavStatus
+            // 
+            this.NavStatus.Name = "NavStatus";
+            this.NavStatus.Size = new System.Drawing.Size(84, 21);
+            this.NavStatus.Text = "Browser Ready";
+            // 
+            // urlBox
+            // 
+            this.urlBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.urlBox.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.urlBox.ForeColor = System.Drawing.Color.Blue;
+            this.urlBox.Location = new System.Drawing.Point(107, 36);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(854, 25);
+            this.urlBox.TabIndex = 4;
+            this.urlBox.Text = "http://";
+            this.urlBox.TextChanged += new System.EventHandler(this.urlBox_TextChanged);
+            this.urlBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.urlBox_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(967, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 26);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // goForward
+            // 
+            this.goForward.BackColor = System.Drawing.Color.White;
+            this.goForward.Image = global::WebBrowser.Properties.Resources.arrow_pointing_to_right;
+            this.goForward.Location = new System.Drawing.Point(44, 36);
+            this.goForward.Name = "goForward";
+            this.goForward.Size = new System.Drawing.Size(26, 26);
+            this.goForward.TabIndex = 7;
+            this.goForward.UseVisualStyleBackColor = false;
+            this.goForward.Click += new System.EventHandler(this.goForward_Click);
+            // 
+            // goBck
+            // 
+            this.goBck.BackColor = System.Drawing.Color.White;
+            this.goBck.Image = global::WebBrowser.Properties.Resources.go_back_arrow;
+            this.goBck.Location = new System.Drawing.Point(12, 36);
+            this.goBck.Name = "goBck";
+            this.goBck.Size = new System.Drawing.Size(26, 26);
+            this.goBck.TabIndex = 6;
+            this.goBck.UseVisualStyleBackColor = false;
+            this.goBck.Click += new System.EventHandler(this.goBck_Click);
+            // 
+            // Refresher
+            // 
+            this.Refresher.BackColor = System.Drawing.Color.White;
+            this.Refresher.Image = global::WebBrowser.Properties.Resources.if_arrow_refresh_441681;
+            this.Refresher.Location = new System.Drawing.Point(75, 36);
+            this.Refresher.Name = "Refresher";
+            this.Refresher.Size = new System.Drawing.Size(26, 26);
+            this.Refresher.TabIndex = 3;
+            this.Refresher.UseVisualStyleBackColor = false;
+            this.Refresher.Click += new System.EventHandler(this.Refresher_Click);
+            // 
+            // Browser_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(1011, 547);
+            this.Controls.Add(this.goForward);
+            this.Controls.Add(this.goBck);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.urlBox);
+            this.Controls.Add(this.Refresher);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Browser_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Turquoise Browser v.0.9";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar ProgressBar;
+        private System.Windows.Forms.ToolStripStatusLabel NavStatus;
+        private System.Windows.Forms.Button Refresher;
+        private System.Windows.Forms.TextBox urlBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button goBck;
+        private System.Windows.Forms.Button goForward;
+    }
+}
+
---
+++ WebBrowser/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class Browser_Form : Form
+    {
+        public Browser_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("CBrowser was developed by Altuğhan Özengi for users of Windows operating system");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Navigate_to_Page();
+        }
+
+        private void Navigate_to_Page()
+        {
+            if (urlBox.Text.Contains(".com")){
+                NavStatus.Text = "Loading: "+ urlBox.Text;
+                webBrowser.Navigate(urlBox.Text);
+            }
+            else
+            {
+                string searchTerm = urlBox.Text.Replace("http://", "");
+                NavStatus.Text = "Loading: " + urlBox.Text;
+                webBrowser.Navigate("http://www.google.com/search?q=" + searchTerm);
+            }
+        }
+
+        private void urlBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Enter)
+            {
+                Navigate_to_Page();
+            }
+        }
+
+        private void urlBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            urlBox.Enabled = true;
+           
+        }
+      
+        private void webBrowser_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            try { 
+                    if (e.CurrentProgress > 0 && e.MaximumProgress > 0) {
+                        ProgressBar.ProgressBar.Value = (int)((double)e.CurrentProgress * 100 / e.MaximumProgress);
+                    }
+                }
+            catch { }
+        
+    }
+
+        private void Refresher_Click(object sender, EventArgs e)
+        {
+            Navigate_to_Page();
+        }
+
+        private void goBck_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+        private void goForward_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+    }
+}
